0958296e13ec8a7b26cd417fa29ecc73e60ff9c5
BenNeighbour <benneighbour007@gmail.com>
2020-03-08 19:21:40 +0000

    Verification Email form hitting the relevant endpoints with the field content

--- src/components/changePassword/body/page.js
+++ src/components/changePassword/body/page.js
@@ -3,12 +3,13 @@ import { withRouter } from 'react-router-dom';
 import ChangePasswordForm from './form/changePasswordForm';
 import { connect } from "react-redux";
 import { getFormValues } from "redux-form";
+import axios from "axios";
 
 class ChangePasswordPage extends React.Component { 
     render() { 
         return (
             <div className="App">
-                <ChangePasswordForm onSubmit={this.props.credentialsPost} />
+                <ChangePasswordForm onSubmit={this.props.credentialsPost.bind(this, "http://localhost:8080/api/v1/user/password/request/change/", this.props.credentials)} />
             </div>
         );
     }
@@ -24,8 +25,11 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
 
     return {
-        credentialsPost: () => dispatch({
-            type: "USER_CHANGE_PASSWORD", payload: null
+        credentialsPost: (url, email) => dispatch({
+            type: "USER_CHANGE_PASSWORD", payload:
+                axios.get(
+                    `${url}${email.email}`
+                )
         })
     }
 };
